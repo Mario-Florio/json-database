1d037cc23005b6356d66525ae4557f7e58c92d9c
ketherball <121590283+ketherball@users.noreply.github.com>
2025-08-20 08:14:56 -0400

    Move deepEqual to shared utils folder

--- __tests__/integration/DocumentRepository.DB/imports.js
+++ __tests__/integration/DocumentRepository.DB/imports.js
@@ -1,10 +1,12 @@
 const { it, assert } = require('../../../shared/testing/test-tools.js');
 const DocumentRepository = require('../../../adapters/repositories/DocumentRepository.js');
 const Document = require('../../../core/entities/Document.js');
+const deepEqual = require('../../../shared/__utils__/deepEqual.js');
 
 module.exports = {
     it,
     assert,
     DocumentRepository,
-    Document
+    Document,
+    deepEqual
 }
