a8759d8a2851e67b2a69d37fcb843e8cfb23dc73
ketherball <121590283+ketherball@users.noreply.github.com>
2025-10-02 11:36:35 -0400

    Update config to handle logger api

--- main.js
+++ main.js
@@ -2,9 +2,17 @@ import Schema from './src/core/entities/Schema.js';
 import model from './src/infrastructure/Model/Model.js';
 import config from './src/config.js';
 
-function setConfig({ DBPATH }) {
-    if (typeof DBPATH !== 'string') throw new Error('DBPATH must be a string');
-    config.DBPATH = DBPATH;
+function setConfig({ DBPATH, LOGGER, LOGGER_PRETTY_PRINT }) {
+    if (DBPATH && typeof DBPATH !== 'string')
+        throw new Error('DBPATH must be a string');
+    if (LOGGER && typeof LOGGER !== 'object')
+        throw new Error('LOGGER must be an object');
+    if (LOGGER_PRETTY_PRINT && typeof LOGGER_PRETTY_PRINT !== 'boolean')
+        throw new Error('LOGGER_PRETTY_PRINT must be a boolean');
+
+    if (DBPATH !== undefined) config.DBPATH = DBPATH;
+    if (LOGGER !== undefined) config.LOGGER = LOGGER;
+    if (LOGGER_PRETTY_PRINT !== undefined) config.LOGGER_PRETTY_PRINT = LOGGER_PRETTY_PRINT;
 }
 
 export default {
--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 const config = {
     DBPATH: './database/collections/',
     ENABLE_CONTRACTS: true,
+    LOGGER: console,
+    LOGGER_PRETTY_PRINT: true
 };
 
 export default config;
