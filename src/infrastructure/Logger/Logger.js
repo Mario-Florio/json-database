7ea54da3e609421b7d3be86eb7527f991692a6c0
ketherball <121590283+ketherball@users.noreply.github.com>
2025-10-03 09:03:20 -0400

    Refactor logger api into a static singleton with distinct components

--- src/infrastructure/Logger/Logger.js
+++ src/infrastructure/Logger/Logger.js
@@ -1,99 +1,35 @@
-import implementsInterface from '../../core/use-cases/__utils__/implementsInterface.js';
-import ILogger from '../../core/ports/ILogger.js';
 import config from '../../config.js';
 
 const BLUE = '\x1b[36m';
 const GREEN = '\x1b[32m';
 const YELLOW = '\x1b[33m';
+const MAGENTA = '\x1b[35m';
 const RED = '\x1b[31m';
 const RESET = '\x1b[0m';
 
 class Logger {
-    #logger;
-    #INFO = `[${BLUE}INFO${RESET}]`;
-    #WARN = `[${YELLOW}WARN${RESET}]`;
-    #ERROR = `[${RED}ERROR${RESET}]`;
+    static #logger = config.LOGGER || console;
 
-    constructor(logger) {
-        if (!logger) {
-            this.#logger = console;
-        } else {
-            if (!implementsInterface(logger, ILogger))
-                throw new Error('Invalid Interface implementation');
-
-            this.#logger = logger;
-        }
-    }
-
-    info(message, meta = {}) {
+    static info(message, meta = {}) {
         const entry = new LogEntry('info', message, meta);
 
-        this.#logger.info(this.#formatEntry(entry));
+        Logger.#logger.info(entry.format());
     }
 
-    warn(message, meta = {}) {
+    static warn(message, meta = {}) {
         const entry = new LogEntry('warn', message, meta);
 
-        this.#logger.warn(this.#formatEntry(entry));
+        Logger.#logger.warn(entry.format());
     }
 
-    error(message, meta = {}) {
+    static error(message, meta = {}) {
         const entry = new LogEntry('error', message, meta);
 
-        this.#logger.error(this.#formatEntry(entry));
-    }
-
-    #formatEntry(entry) {
-        if (config.LOGGER_PRETTY_PRINT === true) {
-            return this.#prettyPrint(entry);
-        } else {
-            return JSON.stringify(entry);
-        }
-    }
-
-    #prettyPrint(object) {
-        if (typeof object !== 'object') {
-            throw new Error('Input must be an object');
-        }
-
-        let prettyStr = '';
-
-        if (object.level === 'info') {
-            prettyStr += this.#INFO;
-        } else if (object.level === 'warn') {
-            prettyStr += this.#WARN;
-        } else if (object.level === 'error') {
-            prettyStr += this.#ERROR;
-        }
-
-        prettyStr += ' ' + object.message;
-
-        prettyStr += ` | ${GREEN}${object.time}${RESET}`;
-
-        const metaKeys = Object.keys(object.meta);
-        if (metaKeys.length === 0) return prettyStr;
-
-        prettyStr += '\n';
-
-        for (const [key, value] of Object.entries(object.meta)) {
-            prettyStr += ` | ${GREEN}${key}${RESET}: ${value}`;
-        }
-
-        return prettyStr;
-    }
-
-    set logger(logger) {
-        if (!implementsInterface(logger, ILogger))
-            throw new Error('Invalid Interface implementation');
-
-        this.#logger = logger;
-    }
-
-    get logger() {
-        return this.#logger;
+        Logger.#logger.error(entry.format());
     }
 }
 
+// UTILS
 class LogEntry {
     constructor(level, message, meta = {}) {
         this.level = level;
@@ -101,6 +37,79 @@ class LogEntry {
         this.time = new Date().toISOString();
         this.meta = meta;
     }
+
+    format() {
+        if (config.LOGGER_PRETTY_PRINT === true) {
+            return prettyPrint(this);
+        } else {
+            return JSON.stringify(this);
+        }
+    }
+}
+
+function prettyPrint(object) {
+    if (typeof object !== 'object') {
+        throw new Error('Input must be an object');
+    }
+
+    let prettyStr = '';
+
+    prettyStr += getPrefixLabel(object.level);
+
+    prettyStr += ' ' + object.message;
+
+    prettyStr += ` | ${GREEN}${object.time}${RESET}`;
+
+    const metaKeys = Object.keys(object.meta);
+    if (metaKeys.length === 0) return prettyStr;
+
+    prettyStr += '\n    ';
+
+    let lineLength = 0;
+    for (const [key, value] of Object.entries(object.meta)) {
+        lineLength += key.length + String(value).length + 5;
+        prettyStr += `${greenWrapper(key)}: ${magentaWrapper(value)} | `;
+
+        if (lineLength > 80) {
+            prettyStr += '\n    ';
+            lineLength = 0;
+        }
+    }
+
+    return prettyStr;
+}
+
+function getPrefixLabel(str) {
+    switch (str.toLowerCase()) {
+        case 'info':
+            return `[${blueWrapper(str.toUpperCase())}]`;
+        case 'warn':
+            return `[${yellowWrapper(str.toUpperCase())}]`;
+        case 'error':
+            return `[${redWrapper(str.toUpperCase())}]`;
+        default:
+            return `[${str.toUpperCase()}]`;
+    }
+}
+
+function blueWrapper(str) {
+    return `${BLUE}${str}${RESET}`;
+}
+
+function magentaWrapper(str) {
+    return `${MAGENTA}${str}${RESET}`;
+}
+
+function greenWrapper(str) {
+    return `${GREEN}${str}${RESET}`;
+}
+
+function yellowWrapper(str) {
+    return `${YELLOW}${str}${RESET}`;
+}
+
+function redWrapper(str) {
+    return `${RED}${str}${RESET}`;
 }
 
 export default Logger;
