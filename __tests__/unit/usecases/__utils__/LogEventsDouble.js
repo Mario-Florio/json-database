3f05199a2767bd19b45cd542f1f9af0306b3f370
Mario <121590283+Mario-Florio@users.noreply.github.com>
2025-10-03 23:17:01 -0400

    Fix LogEventDouble to have appropriate state and methods

--- __tests__/unit/usecases/__utils__/LogEventsDouble.js
+++ __tests__/unit/usecases/__utils__/LogEventsDouble.js
@@ -1,6 +1,18 @@
 import EventEmitter from 'node:events';
 
-class LogEventEmitterDouble extends EventEmitter {}
+class LogEventEmitterDouble extends EventEmitter {
+    events = {
+        ATTEMPT: 'ATTEMPT',
+        SUCCESS: 'SUCCESS',
+        FAILED: 'FAILED',
+        CORE: 'CORE',
+        ERROR: 'ERROR',
+    };
+
+    emit(event, ...args) {
+        return;
+    }
+}
 
 const logEventEmitterDouble = new LogEventEmitterDouble();
 logEventEmitterDouble.removeAllListeners();
